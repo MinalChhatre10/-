28bbf272f9a8d6dda42c6e36b52446d7f8016dee
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-26 14:55:19 +0530

    layouts-navbar,footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Footer from "@/components/footer/footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>Main Layout</h1>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
