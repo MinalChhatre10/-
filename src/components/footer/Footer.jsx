421e4e13e9bbc48715661e18253cebe21b656d4e
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-27 14:39:08 +0530

    name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <div>@2023 MinalChhatre. All rights reserved.</div>
+      <div>@2023 immeenal. All rights reserved.</div>
     </div>
   );
 };
