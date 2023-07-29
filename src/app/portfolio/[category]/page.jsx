b1c3010ae908082a2d64ccf9dc7820768b3037d0
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-29 18:06:11 +0530

    portfolio category

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import styles from "./page.module.css";
-const Category = () => {
-  return <div>Category</div>;
+import Button from "@/components/buttons/button";
+import Image from "next/image";
+
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>test</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src="" alt="" />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>test</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
