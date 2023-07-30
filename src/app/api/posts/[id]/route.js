678fe8ef0fe009d208d8face218dfcf69577a43a
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-30 11:59:34 +0530

    id-post (route.js)

---
+++ src/app/api/posts/[id]/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import connect from "@/utils/db";
+import Post from "@/models/Post";
+import mongoose from "mongoose";
+
+// Set the strictQuery option as needed
+mongoose.set("strictQuery", false); // or true, based on your preference
+console.log(process.env.MONGO);
+export const GET = async (request) => {
+  try {
+    await connect();
+
+    const posts = await Post.find();
+
+    return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
