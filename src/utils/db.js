0ba376fe4fa5e995dad0a8fcc44c9735761c1de8
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-30 11:32:49 +0530

    connection successful with db

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB Atlas");
   } catch (error) {
+    console.error("Connection Error:", error.message);
     throw new Error("Connection failed!");
   }
 };
