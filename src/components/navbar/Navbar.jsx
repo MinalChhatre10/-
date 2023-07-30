e1b80ebf53f4c81232210996a546cf6e998590c2
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-30 15:48:20 +0530

    navbar session signout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,10 @@
 
 import Link from "next/link";
 import React from "react";
-import styles from "./page.module.css";
+import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
+
 const links = [
   {
     id: 1,
@@ -37,26 +40,26 @@ const links = [
 ];
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        CharityTales
+        lamamia
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
-      <button
-        onClick={() => {
-          console.log("Logged out");
-        }}
-        className={styles.logout}
-      >
-        Logout
-      </button>
     </div>
   );
 };
