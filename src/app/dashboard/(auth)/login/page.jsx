80042dc1f5f0f93175b15905c726cfa4f1896a25
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-30 15:27:55 +0530

    done login page

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { getProviders, signIn, useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 const Login = () => {
-  return <div>login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", {
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+        className={styles.button + " " + styles.google}
+      >
+        Login with Google
+      </button>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/register">
+        Create new account
+      </Link>
+    </div>
+  );
 };
 
 export default Login;
