446a0f8694382e14205898889fa7dd2104941fa3
MinalChhatre10 <minalchhatre10@gmail.com>
2023-07-30 16:34:10 +0530

    posts and post api added

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,11 +1,7 @@
 import { NextResponse } from "next/server";
 import connect from "@/utils/db";
 import Post from "@/models/Post";
-import mongoose from "mongoose";
 
-// Set the strictQuery option as needed
-mongoose.set("strictQuery", false); // or true, based on your preference
-//console.log(process.env.MONGO);
 export const GET = async (request) => {
   const url = new URL(request.url);
 
@@ -17,6 +13,22 @@ export const GET = async (request) => {
     const posts = await Post.find(username && { username });
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  const body = await request.json();
+
+  const newPost = new Post(body);
+
+  try {
+    await connect();
+
+    await newPost.save();
+
+    return new NextResponse("Post has been created", { status: 201 });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
